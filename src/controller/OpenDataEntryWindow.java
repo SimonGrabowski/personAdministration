4343459bb38950e30f153e62902cc420e296df2f
Simon <simongrabowski.andere1@gmail.com>
2023-01-04 11:08:06 +0100

    Layout hinzugefügt für OpenDataEntryWindow

--- src/controller/OpenDataEntryWindow.java
+++ src/controller/OpenDataEntryWindow.java
@@ -1,39 +1,76 @@
 package controller;
 
-import gui.MainFrame;
-
 import javax.swing.*;
 import java.awt.*;
 
 public class OpenDataEntryWindow {
 
+    private static JTextField inputFirstname, inputLastname, inputAddress, inputSalary, inputJob, inputAge = new JTextField(20);
+    private static JLabel labelFirstname, labelLastname, labelAddress, labelSalary, labelJob, labelAge;
+
     public static void startDataEntryFrame() {
         JFrame dataEntryFrame = new JFrame();
         JPanel dataEntryPanel = new JPanel();
-        dataEntryPanel.setSize(400,400);
-        dataEntryPanel.setLayout(null);
+        dataEntryPanel.setSize(400, 400);
+        GridBagLayout gridBagLayout = new GridBagLayout();
+        GridBagConstraints gridBagConstraints = new GridBagConstraints();
+        dataEntryPanel.setLayout(gridBagLayout);
 
-        String[] labels = {"Firstname", "Lastname", "Address", "Salary", "Age", "Job"};
+        inputFirstname = new JTextField(20);
+        inputLastname = new JTextField(20);
+        inputAddress = new JTextField(20);
+        inputSalary = new JTextField(20);
+        inputJob = new JTextField(20);
+        inputAge = new JTextField(20);
 
-        JLabel labelFirstname = new JLabel("Firstname:", 10);
-        JLabel labelNachname = new JLabel("Nachname:");
-        JLabel labelAddress = new JLabel("Address:");
-        JLabel labelSalary = new JLabel("Salary:");
-        JLabel labelJob = new JLabel("Job:");
-        JLabel labelAge = new JLabel("Age:");
+        labelFirstname = new JLabel("Firstname:");
+        labelLastname = new JLabel("Lastname:");
+        labelAddress = new JLabel("Address:");
+        labelSalary = new JLabel("Salary");
+        labelJob = new JLabel("Job:");
+        labelAge = new JLabel("Age:");
 
-        JTextField inputFirstname = new JTextField(32);
-        JTextField inputNachname = new JTextField(32);
-        JTextField inputAddress = new JTextField(32);
-        JTextField inputSalary = new JTextField(32);
-        JTextField inputJob = new JTextField(32);
-        JTextField inputAge = new JTextField(32);
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridy = 0;
+        dataEntryPanel.add(labelFirstname, gridBagConstraints);
+        gridBagConstraints.gridx = 1;
+        gridBagConstraints.gridy = 0;
+        dataEntryPanel.add(inputFirstname, gridBagConstraints);
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridy = 1;
+        dataEntryPanel.add(labelLastname, gridBagConstraints);
+        gridBagConstraints.gridx = 1;
+        gridBagConstraints.gridy = 1;
+        dataEntryPanel.add(inputLastname, gridBagConstraints);
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridy = 2;
+        dataEntryPanel.add(labelAddress, gridBagConstraints);
+        gridBagConstraints.gridx = 1;
+        gridBagConstraints.gridy = 2;
+        dataEntryPanel.add(inputAddress, gridBagConstraints);
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridy = 3;
+        dataEntryPanel.add(labelSalary, gridBagConstraints);
+        gridBagConstraints.gridx = 1;
+        gridBagConstraints.gridy = 3;
+        dataEntryPanel.add(inputSalary, gridBagConstraints);
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridy = 4;
+        dataEntryPanel.add(labelJob, gridBagConstraints);
+        gridBagConstraints.gridx = 1;
+        gridBagConstraints.gridy = 4;
+        dataEntryPanel.add(inputJob, gridBagConstraints);
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridy = 5;
+        dataEntryPanel.add(labelAge, gridBagConstraints);
+        gridBagConstraints.gridx = 1;
+        gridBagConstraints.gridy = 5;
+        dataEntryPanel.add(inputAge, gridBagConstraints);
 
         dataEntryFrame.add(dataEntryPanel);
 
         dataEntryFrame.setSize(400, 400);
         dataEntryFrame.setResizable(false);
-        dataEntryFrame.setLayout(null);
         dataEntryFrame.setVisible(true);
     }
 }
